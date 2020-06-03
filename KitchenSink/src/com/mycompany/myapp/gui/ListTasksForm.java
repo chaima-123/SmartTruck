c8b60c86bb03f34c00c51c9eae97abceeeda0ed0
imennnn <imen.elabed1@gmail.com>
2020-06-03 21:39:33 +0100

    update

--- KitchenSink/src/com/mycompany/myapp/gui/ListTasksForm.java
+++ KitchenSink/src/com/mycompany/myapp/gui/ListTasksForm.java
@@ -18,10 +18,14 @@ import com.codename1.ui.Dialog;
 import com.codename1.ui.FontImage;
 import com.codename1.ui.Form;
 import com.codename1.ui.Label;
+import com.codename1.ui.List;
 import com.codename1.ui.TextField;
+import com.codename1.ui.Toolbar;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.list.DefaultListModel;
 import com.codename1.ui.plaf.Border;
 import com.codename1.ui.spinner.Picker;
 import com.mycompany.myapp.entities.Article;
@@ -45,6 +49,45 @@ public class ListTasksForm extends Form {
         palettes = ServicePalette.getInstance().getAllPAlette();
         Form hi = new Form();
         Container c = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        TextField rech = new TextField("", "Rechercher un livreur");
+        Container cn = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        Container cnToll = new Container(new BorderLayout());
+        TextField zoneRecherche = new TextField();
+        zoneRecherche.setHint("Rechercher par nom");
+        Button boutonRecherche = new Button("ok");
+        cnToll.addComponent(BorderLayout.CENTER, zoneRecherche);
+        cnToll.addComponent(BorderLayout.EAST, boutonRecherche);
+        Toolbar toolbar = new Toolbar();
+        setToolbar(toolbar);
+        toolbar.setTitleComponent(cnToll);
+
+        boutonRecherche.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                Form f = new Form();
+
+                cn.setScrollableY(true);
+                ArrayList<Palette> l0 = ServicePalette.getInstance().findPalette(Integer.parseInt(zoneRecherche.getText()));
+                for (int i = 0; i < l0.size(); i++) {
+                    DefaultListModel model2 = new DefaultListModel();
+                    model2.addItem("Numero de la palette: "+l0.get(i).getNum_lot());
+model2.addItem("Quantite: "+l0.get(i).getQte());
+model2.addItem("Reference de l'article: "+l0.get(i).getRef());
+model2.addItem("Emplacement: "+l0.get(i).getCodeEmp());
+model2.addItem("Date d'expiration: "+l0.get(i).getDate_expiration());
+
+
+                    List liste = new List(model2);
+                    getStyle().setBgColor(0xffffff);
+                    f.addComponent(liste);
+                    f.getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK, e -> previous.show());
+                    f.getToolbar().setTitle(("Resultats de la recherche"));
+                    f.show();
+
+                }
+
+            }
+        });
 
         for (Palette p : palettes) {
             Label l = new Label("Numero de lot: " + Integer.toString(p.getNum_lot()));
@@ -72,16 +115,16 @@ public class ListTasksForm extends Form {
                     Container c1 = new Container(new BoxLayout(BoxLayout.Y_AXIS));
 
                     Label lqte = new Label("Quantite: ");
-                    TextField tfName = new TextField(""+(Integer.toString(p.getQte())));
+                    TextField tfName = new TextField(Integer.toString(p.getQte()));
                    // tfName.setText(Integer.toString(p.getQte()));
                     Label ldate = new Label("Date d'expiration: ");
                     Picker dateP = new Picker();
-                    dateP.setDate(p.getDate_expiration());
+                   // dateP.setDate(p.getDate_expiration());
                     // Date value = (Date) dateP.getValue();
                     Label larticle = new Label("Article: ");
 
-                    ComboBox cmbArticle = new ComboBox();
-                    ComboBox cmbCode = new ComboBox();
+                    ComboBox<String> cmbArticle = new ComboBox();
+                    ComboBox<String> cmbCode = new ComboBox();
 
                     Label lcode = new Label("Emplacement: ");
 
@@ -105,8 +148,13 @@ public class ListTasksForm extends Form {
                     Button btnS = new Button("Supprimer");
                     btnS.getAllStyles().setBorder(Border.createGrooveBorder(CENTER, 0x189fA5), focusScrolling);
                     btnS.getAllStyles().setFgColor(0x189fA5);
-
-                    Palette P = new Palette((p.getNum_lot()), Integer.parseInt(tfName.getText()), (Date) dateP.getValue(), (String) cmbArticle.getSelectedItem(), (String) cmbCode.getSelectedItem());
+                    Date value = (Date) dateP.getValue();
+                        
+                    //String sDate1="2020-06-10";  
+                   
+                    //Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
+                    
+                    Palette P = new Palette((p.getNum_lot()), 1000, (Date) dateP.getValue(),  cmbArticle.getSelectedItem(),  cmbCode.getSelectedItem());
 
                     btnM.addActionListener(new ActionListener() {
                         @Override
@@ -115,7 +163,7 @@ public class ListTasksForm extends Form {
                                 Dialog.show("Erreur", "Tous les champs sont obligatoires", new Command("OK"));
                             } else {
                                 if (ServicePalette.getInstance().UpdatePalette(P)) {
-
+                                    System.out.println(value);
                                     Dialog.show("Modification", "Palette modifiee avec succes", "ok", null);
 
                                 } else {
