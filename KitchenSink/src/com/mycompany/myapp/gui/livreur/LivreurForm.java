6227536bd6d98fd15a30e867bc3d76f6a7431604
Hanene1 <hanene.ennine@esprit.tn>
2020-06-07 23:05:29 +0100

    Update LivreurForm.java

--- KitchenSink/src/com/mycompany/myapp/gui/livreur/LivreurForm.java
+++ KitchenSink/src/com/mycompany/myapp/gui/livreur/LivreurForm.java
@@ -6,9 +6,11 @@
 package com.mycompany.myapp.gui.livreur;
 
 import com.codename1.ui.Button;
+import static com.codename1.ui.Component.CENTER;
 import com.codename1.ui.Form;
 import com.codename1.ui.Label;
 import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.plaf.Border;
 
 /**
  *
@@ -18,7 +20,7 @@ public class LivreurForm extends Form {
 
     Form current;
 
-    public LivreurForm() { //Form previous
+    public LivreurForm() { 
 
         current = this;
         setTitle("Gestion des livreurs");
@@ -28,12 +30,17 @@ public class LivreurForm extends Form {
         Button btnListLivreurs = new Button("Afficher les livreurs");
         Button btnAddLivreur = new Button("Ajouter un livreur");
 
+        btnListLivreurs.getAllStyles().setBorder(Border.createGrooveBorder(CENTER, 0x189fA5), focusScrolling);
+        btnListLivreurs.getAllStyles().setFgColor(0x189fA5);
+        
+        btnAddLivreur.getAllStyles().setBorder(Border.createGrooveBorder(CENTER, 0x189fA5), focusScrolling);
+        btnAddLivreur.getAllStyles().setFgColor(0x189fA5);
+        
         btnAddLivreur.addActionListener(e -> new AddLivreurForm(current).show());
         btnListLivreurs.addActionListener(e -> new ListLivreursForm(current).show());
 
         add(btnAddLivreur);
         add(btnListLivreurs);
 
-        //getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK, e -> previous.showBack());
     }
 }
