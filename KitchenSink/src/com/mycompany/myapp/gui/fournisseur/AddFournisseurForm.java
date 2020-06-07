d2d659cffbdd6eb4ef9ffef69a40d6d7acdfdf87
Hanene1 <hanene.ennine@esprit.tn>
2020-06-07 23:04:50 +0100

    Update AddFournisseurForm.java

--- KitchenSink/src/com/mycompany/myapp/gui/fournisseur/AddFournisseurForm.java
+++ KitchenSink/src/com/mycompany/myapp/gui/fournisseur/AddFournisseurForm.java
@@ -1,7 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.mycompany.myapp.gui.fournisseur;
 
 import com.codename1.ui.Button;
 import com.codename1.ui.Command;
+import static com.codename1.ui.Component.CENTER;
 import com.codename1.ui.Dialog;
 import com.codename1.ui.FontImage;
 import com.codename1.ui.Form;
@@ -9,6 +15,7 @@ import com.codename1.ui.TextField;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.plaf.Border;
 import com.mycompany.myapp.entities.Fournisseur;
 import com.mycompany.myapp.services.ServiceFournisseur;
 
@@ -30,15 +37,19 @@ public class AddFournisseurForm extends Form {
         TextField tfFax = new TextField("", "Fax du fournisseur");
 
         Button btnValider = new Button("Ajouter fournisseur");
+        btnValider.getAllStyles().setBorder(Border.createGrooveBorder(CENTER, 0x189fA5), focusScrolling);
+        btnValider.getAllStyles().setFgColor(0x189fA5);
         btnValider.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent evt) {
-              
+              if ((tfNomSociete.getText().length() == 0) || (tfCin.getText().length()<7) || (tfAdresse.getText().length() == 0) || (tfEmail.getText().length() == 0)  || (tfTelephone.getText().length()<8) || (tfFax.getText().length()<8)) {
+                        Dialog.show("Alert", "Veuillez remplir tous les champs","OK", null);
+                    } else {
                     try {
                         Fournisseur l = new Fournisseur(Integer.parseInt(tfCin.getText()), tfEmail.getText(), tfAdresse.getText(), Integer.parseInt(tfTelephone.getText()), Integer.parseInt(tfFax.getText()), tfNomSociete.getText());
                         if (ServiceFournisseur.getInstance().addFournisseur(l)) {
                             Dialog.show("Success", "Fournisseur ajouté avec succés","OK",null);
-			    new ListFournisseursForm(previous).show();
+                            new ListFournisseursForm(previous).show();
                         } else {
                            Dialog.show("ERREUR", "Echec à l'ajout", "OK", null);
                         }
@@ -46,7 +57,7 @@ public class AddFournisseurForm extends Form {
                         Dialog.show("ERREUR", "ça doit être un numero", "OK", null);
                     }
 
-                
+              }
             }
         });
 
